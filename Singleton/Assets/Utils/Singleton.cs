eb63dd80f942edac642ac62ee39a0839179cb705
Roman Knyazev <roman.knyazev@omega-r.com>
2019-02-21 15:13:50 +0300

    Add singleton

---
+++ Singleton/Assets/Utils/Singleton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        public static T Instance { get; private set; }
+
+        public Singleton()
+        {
+            if (Instance != null)
+                Debug.Log("Singleton " + typeof(T) + " already exists -- overwriting");
+            Instance = this as T;
+        }
+    }
+}
